fec1c19aa5b9475e63864250e2e473f95158f634
bpalominosg <ben.palominos@duocuc.cl>
2026-06-24 18:14:09 -0400

    cambio insert ventas-despacho

--- back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/target/classes/data.sql
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/target/classes/data.sql
@@ -1,5 +1,4 @@
--- Insertamos 2 despachos de prueba que corresponden a las ventas generadas arriba
-INSERT INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
+INSERT INTO despacho (fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, entregado)
 VALUES 
-(1, '2026-06-21', 'AB-CD-12', 1, 1, 'Avenida Siempreviva 742, Santiago', 15000, false),
-(2, '2026-06-22', 'ZZ-XX-99', 2, 2, 'Calle Falsa 123, Providencia', 45000, true);
+('2026-06-21', 'AB-CD-12', 1, 1, 'Avenida Siempreviva 742, Santiago', 15000, false),
+('2026-06-22', 'ZZ-XX-99', 2, 2, 'Calle Falsa 123, Providencia', 45000, true);
--- back-Ventas_SpringBoot/Springboot-API-REST/src/main/resources/data.sql
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES ('Av. Vitacura 1234, Santiago', 45000, '2026-06-20', false);
+VALUES ('Av. Vitacura 1234, Santiago', 45000, '2026-06-20', 0);
 
 INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES ('Calle Los Olivos 567, Concepción', 12500, '2026-06-22', false);
+VALUES ('Calle Los Olivos 567, Concepción', 12500, '2026-06-22', 0);
 
 INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES ('Pasaje El Sol 89, Viña del Mar', 89990, '2026-06-23', true);
+VALUES ('Pasaje El Sol 89, Viña del Mar', 89990, '2026-06-23', 1);
 
 INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES ('Av. El Parrón 4321, La Cisterna', 32000, '2026-06-24', false);
+VALUES ('Av. El Parrón 4321, La Cisterna', 32000, '2026-06-24', 0);
