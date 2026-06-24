5e248553367d423d5ee6aa8b1188167a48b3ab07
bpalominosg <ben.palominos@duocuc.cl>
2026-06-24 17:48:03 -0400

    cambios properties

--- back-Ventas_SpringBoot/Springboot-API-REST/target/classes/data.sql
+++ back-Ventas_SpringBoot/Springboot-API-REST/target/classes/data.sql
@@ -1,6 +1,8 @@
--- Insertamos 3 ventas de prueba
-INSERT INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES 
-(1, 'Avenida Siempreviva 742, Santiago', 15000, '2026-06-20', true),
-(2, 'Calle Falsa 123, Providencia', 45000, '2026-06-21', true),
-(3, 'Pasaje Los Aromos 456, Maipú', 8500, '2026-06-22', false);
+INSERT IGNORE INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
+VALUES (1, '2026-05-21', 'BCDF-56', 1, 1, 'Av. Los Leones 1234, Ñuñoa', 15000, true);
+
+INSERT IGNORE INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
+VALUES (2, '2026-05-22', 'KLMN-89', 0, 2, 'Gran Avenida 7890, El Bosque', 25000, false);
+
+INSERT IGNORE INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
+VALUES (3, '2026-05-24', 'XYZW-12', 2, 3, 'Av. Irarrazaval 3000, Ñuñoa', 12500, false);
