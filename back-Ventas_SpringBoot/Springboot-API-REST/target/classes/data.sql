c9b600095461d49f8cb744bd39b60cb174406089
bpalominosg <ben.palominos@duocuc.cl>
2026-06-24 17:54:47 -0400

    base de datos ventas

--- back-Ventas_SpringBoot/Springboot-API-REST/target/classes/data.sql
+++ back-Ventas_SpringBoot/Springboot-API-REST/target/classes/data.sql
@@ -1,8 +1,11 @@
-INSERT IGNORE INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
-VALUES (1, '2026-05-21', 'BCDF-56', 1, 1, 'Av. Los Leones 1234, Ñuñoa', 15000, true);
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Av. Vitacura 1234, Santiago', 45000, '2026-06-20', false);
 
-INSERT IGNORE INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
-VALUES (2, '2026-05-22', 'KLMN-89', 0, 2, 'Gran Avenida 7890, El Bosque', 25000, false);
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Calle Los Olivos 567, Concepción', 12500, '2026-06-22', false);
 
-INSERT IGNORE INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
-VALUES (3, '2026-05-24', 'XYZW-12', 2, 3, 'Av. Irarrazaval 3000, Ñuñoa', 12500, false);
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Pasaje El Sol 89, Viña del Mar', 89990, '2026-06-23', true);
+
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Av. El Parrón 4321, La Cisterna', 32000, '2026-06-24', false);
